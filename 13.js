29fbba70c44766c7240a83bbdb31072061720bd9
unknown <619165002@qq.com>
2019-10-31 18:14:27 +0800

    类

---
+++ 13.js
@@ -0,0 +1,10 @@
+// 类
+var Animal = /** @class */ (function () {
+    function Animal(name) {
+        this.name = name;
+    }
+    return Animal;
+}());
+var cat = new Animal("cat");
+cat.name = "656565";
+console.log(cat.name);
--- 13.ts
+++ 13.ts
@@ -1,1 +1,11 @@
 // 类
+class Animal {
+    private name; 
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+let cat = new Animal("cat");
+cat.name = "656565"
+console.log(cat.name);
